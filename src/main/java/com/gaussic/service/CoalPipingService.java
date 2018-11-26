7e9e18099fede66027c0cb84b3d9b8dbac4bd0f2
summerJY100 <15210365768@163.com>
2018-11-26 14:59:40 +0800

    添加java8的lamda表达式代码优化

--- src/main/java/com/gaussic/service/CoalPipingService.java
+++ src/main/java/com/gaussic/service/CoalPipingService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CoalPipingService {
@@ -27,16 +28,17 @@ public class CoalPipingService {
 
     /**
      * 查询所有的管道，并且关联pipingSet对象
+     *
      * @return
      */
-    public List<CoalPipingEntity> findAll(){
+    public List<CoalPipingEntity> findAll() {
         List<CoalPipingEntity> coalPipingEntityList = coalPipingRepository.findAll();
         List<CoalPipingSetEntity> coalPipingSetEntityList = coalMillPipingSetRepository.findAll();
-        for(CoalPipingEntity coalPipingEntity:coalPipingEntityList){
+        for (CoalPipingEntity coalPipingEntity : coalPipingEntityList) {
             Long coalPipingId = coalPipingEntity.getId();
-            for(CoalPipingSetEntity coalPipingSetEntity:coalPipingSetEntityList){
+            for (CoalPipingSetEntity coalPipingSetEntity : coalPipingSetEntityList) {
                 Long coalPipingSetPipingId = coalPipingSetEntity.getCoalPipeId();
-                if(coalPipingId.equals(coalPipingSetPipingId)){
+                if (coalPipingId.equals(coalPipingSetPipingId)) {
                     coalPipingEntity.setCoalPipingSetEntity(coalPipingSetEntity);
                     break;
                 }
@@ -47,21 +49,21 @@ public class CoalPipingService {
 
 
     /**
+     * @param coalMillEntityList
      * @Description 设置coalpipingEntity对象的通讯中断告警对象
      * 遍历告警信息，将  ”粉管通讯中断”  的告警信息查询出来,并设置到coalPipingEntity中
      * 查询当前粉管是否存在通讯中断告警，并且是正在告警中的状态。设置完成后，coalPipingEntity将存在alarmHistoryEntity对象。
      * @Author jiangyong xia
      * @Date 10:36 18/9/3
-     * @param coalMillEntityList
      **/
     public void handleCoalPipingWithAlarmForCommunication(List<CoalMillEntity> coalMillEntityList) {
-        List<AlarmHistoryEntity> alarmHistoryEntityList = alarmHistoryRepository.findByAAlarmTypeAndAAlarmState(AlarmHistoryEntity.ALARM_TYPE_COMMUNICATION_PIPE,1);
-        for(CoalMillEntity coalMillEntity:coalMillEntityList){
-            for(CoalPipingEntity coalPipingEntity:coalMillEntity.getCoalPipingEntityList()){
-                if(null != alarmHistoryEntityList){
-                    for(AlarmHistoryEntity alarmHistoryEntity:alarmHistoryEntityList){
-                        if(null != alarmHistoryEntity.getaAlarmPipeId()){
-                            if(alarmHistoryEntity.getaAlarmPipeId().equals(coalPipingEntity.getId())){
+        List<AlarmHistoryEntity> alarmHistoryEntityList = alarmHistoryRepository.findByAAlarmTypeAndAAlarmState(AlarmHistoryEntity.ALARM_TYPE_COMMUNICATION_PIPE, 1);
+        for (CoalMillEntity coalMillEntity : coalMillEntityList) {
+            for (CoalPipingEntity coalPipingEntity : coalMillEntity.getCoalPipingEntityList()) {
+                if (null != alarmHistoryEntityList) {
+                    for (AlarmHistoryEntity alarmHistoryEntity : alarmHistoryEntityList) {
+                        if (null != alarmHistoryEntity.getaAlarmPipeId()) {
+                            if (alarmHistoryEntity.getaAlarmPipeId().equals(coalPipingEntity.getId())) {
                                 coalPipingEntity.setAlarmHistoryEntity(alarmHistoryEntity);
                                 break;
                             }
@@ -70,5 +72,48 @@ public class CoalPipingService {
                 }
             }
         }
+
+/*
+
+        coalMillEntityList.stream().flatMap(coalMillEntity -> coalMillEntity.getCoalPipingEntityList().stream())
+                .forEach(coalPipingEntity -> System.out.println("告警记录：" + coalPipingEntity.getId() +"," +
+                        "alarmHistory:"+coalPipingEntity
+                        .getAlarmHistoryEntity
+                        ()));
+
+*/
+
+/*
+        使用java8的lamda表达式进行处理
+        coalMillEntityList.stream().
+                flatMap(coalMillEntity -> coalMillEntity.getCoalPipingEntityList().stream()).forEach(coalPipingEntity ->
+                coalPipingEntity.setAlarmHistoryEntity(
+                        alarmHistoryEntityList.stream().
+                                filter(alarmHistoryEntity -> alarmHistoryEntity.getaAlarmPipeId().equals(coalPipingEntity.getId())).
+                                findFirst().orElse(null)));
+        */
+
+//        coalMillEntityList.stream().filter((c)-> c.getCoalPipingEntityList()!=null).collect(Collectors.toList());
+    }
+
+
+    public void handleCoalPipingWithAlarmForCommunicationForLamda(List<CoalMillEntity> coalMillEntityList) {
+        List<AlarmHistoryEntity> alarmHistoryEntityList = alarmHistoryRepository.findByAAlarmTypeAndAAlarmState(AlarmHistoryEntity.ALARM_TYPE_COMMUNICATION_PIPE, 1);
+        List<AlarmHistoryEntity> alarmHistoryEntityList1 = alarmHistoryEntityList.stream().filter(a -> a.getaAlarmPipeId() != null).collect(Collectors.toList());
+        for (CoalMillEntity coalMillEntity : coalMillEntityList) {
+//            for (CoalPipingEntity coalPipingEntity : coalMillEntity.getCoalPipingEntityList()) {
+//                AlarmHistoryEntity alarmHistoryEntity = alarmHistoryEntityList1.stream().filter(alarmEntity -> alarmEntity.getaAlarmPipeId().equals(coalMillEntity.getId())).findFirst().orElse(null);
+//                coalPipingEntity.setAlarmHistoryEntity(alarmHistoryEntity);
+//            }
+            coalMillEntity.getCoalPipingEntityList().forEach(coalPipingEntity->{
+                AlarmHistoryEntity alarmHistoryEntity = alarmHistoryEntityList1.stream().filter(alarmEntity -> alarmEntity.getaAlarmPipeId().equals(coalMillEntity.getId())).findFirst().orElse(null);
+                coalPipingEntity.setAlarmHistoryEntity(alarmHistoryEntity);
+            });
+        }
+
+
+
+//        coalMillEntityList.stream().flatMap(coalMill->coalMill.getCoalPipingEntityList().stream()).filter()
+//        coalMillEntityList.stream().filter((c)-> c.getCoalPipingEntityList()!=null).collect(Collectors.toList());
     }
 }

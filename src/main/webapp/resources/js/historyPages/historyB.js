3b6fac9acaf1b7cb2d835f391a2d15f96539c24f
summerJY100 <15210365768@163.com>
2018-12-07 16:00:45 +0800

    美化前台

--- src/main/webapp/resources/js/historyPages/historyB.js
+++ src/main/webapp/resources/js/historyPages/historyB.js
@@ -1,11 +1,11 @@
-function getQueryUrl(){
+function getQueryUrl() {
     var $forceSetMillValue = $("#forceSetMillValue");
     var url = "../getMillHistoryDataWithMill?mill=B";
-    if( $forceSetMillValue[0].checked === true){
+    if ($forceSetMillValue[0].checked === true) {
         var millValue = $("#millValue").val();
-        url = url + "&millValue="+millValue;
+        url = url + "&millValue=" + millValue;
     }
 
 
-
     return url;
+}
